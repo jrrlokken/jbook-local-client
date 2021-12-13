a79b9a9b30a2a794d19b8eebf7a5551fd0cbf96f
Joshua Lokken <joshualokken@pm.me>
2021-12-13 13:51:58 -0600

    Finish styling

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,7 +1,9 @@
-import { Fragment } from 'react';
-import { useTypedSelector } from '../hooks/use-typed-selector';
-import CellListItem from './cell-list-item';
-import AddCell from './add-cell';
+import { Fragment } from "react";
+import { useTypedSelector } from "../hooks/use-typed-selector";
+import CellListItem from "./cell-list-item";
+import AddCell from "./add-cell";
+
+import "./cell-list.css";
 
 const CellList: React.FC = () => {
   const cells = useTypedSelector(({ cells: { order, data } }) =>
@@ -16,7 +18,7 @@ const CellList: React.FC = () => {
   ));
 
   return (
-    <div>
+    <div className="cell-list">
       <AddCell forceVisible={cells.length === 0} previousCellId={null} />
       {renderedCells}
     </div>
